a51c9af8b6780ff383e5d2380dfa71c44de752bd
ASP-03 <ankit20038@gmail.com>
2025-02-01 10:30:27 +0530

    Fixes in page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -8,17 +8,26 @@ export default function RegisterPage() {
     const[password, setPassword] = useState('');
     const[creatingUser, setCreatingUser] = useState(false);
     const[userCreated, setUserCreated] = useState(false);
+    const[error, setError] = useState(false);
     async function handleFormSubmit(ev){
         ev.preventDefault();
         setCreatingUser(true);
-        await fetch('/api/register', {
-            method: 'POST',
-            body: JSON.stringify({email, password}),
-            headers: {'Content-Type': 'application/json'},
-        })
-        setCreatingUser(false);
-        setUserCreated(true);
-    }
+        setError(false);
+        setUserCreated(false);
+        const response = await fetch('/api/register', {
+                method: 'POST',
+                body: JSON.stringify({email, password}),
+                headers: {'Content-Type': 'application/json'},
+            })
+            if(response.ok) {
+                setUserCreated(true);
+            }
+            else {
+                setError(true);
+            }
+            setCreatingUser(false);
+        
+  }
     return(
         <section className="mt-8">
             <h1 className="mb-4 text-center text-red-600 text-4xl">
@@ -26,8 +35,14 @@ export default function RegisterPage() {
             </h1>
             {userCreated && (
                 <div className="my-4 text-center">
-                    Registered! Now you can{' '}
+                    Registered!<br /> Now you can{' '}
                     <Link className='underline' href={'/login'}>Login &raquo;</Link>
+                </div>
+            )}
+            {error && (
+                <div className="my-4 text-center">
+                    An error has occured.<br />
+                    Please try again later.
                 </div>
             )}
             <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
