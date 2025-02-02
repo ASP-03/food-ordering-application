902d680aa7fc695811196b74e325402c1ae5c6f5
ASP-03 <ankit20038@gmail.com>
2025-02-03 02:45:26 +0530

    changes in some buttons

--- src/app/components/layout/Hero.js
+++ src/app/components/layout/Hero.js
@@ -16,11 +16,11 @@ export default function Hero() {
               <p className="my-6 text-gray-500 text-sm">Pizza makes everything better—whether it's solving problems or celebrating victories. Do you have a favorite pizza topping, or are you just vibing with the pizza energy?
               </p>
               <div className="flex max-w-xs gap-4">
-                <button className="bg-red-600 flex items-center gap-1 text-white px-6 py-2 round-full text-sm"> 
+                <button className="bg-red-600 flex justify items-center gap-1 text-white px-6 py-2 round-full text-sm"> 
                  ORDER NOW
                  <Right/>
                 </button>
-                <button className="flex gap-1 items-center px-2 py-2 text-gray-600 font-semibold">
+                <button className="flex border-0 gap-1 items-center px-2 py-2 text-gray-600 font-semibold">
                     Learn more
                     <Right />
                 </button>
