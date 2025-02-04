f5499b89e6fbf07e5e10ab2fc661e2d3f3a12c75
ASP-03 <ankit20038@gmail.com>
2025-02-05 00:16:07 +0530

    Fixes

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,22 +1,9 @@
-import bcrypt from 'bcrypt';
 import { model, models, Schema } from "mongoose";
 
 const UserSchema = new Schema({
+    name: {type: String},
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true, 
-        validate: pass => {
-            if (!pass?.length || pass.length < 5) {
-                new Error('Password must contain at least 5 characters');
-                return false;
-            }
-    } 
-  }
- } , {timestamps: true})
-
-UserSchema.post('validate', function(user) {
-    const notHashpass = user.password;
-    const salt = bcrypt.genSaltSync(10);
-    user.password = bcrypt.hashSync(notHashpass, salt);
-})
+    password: {type: String},
+ }, {timestamps: true})
 
 export const User = models?.User || model('User', UserSchema);
