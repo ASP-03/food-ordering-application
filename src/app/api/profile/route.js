29434daa97456004ce0c279645751d636eaaf212
ASP-03 <ankit20038@gmail.com>
2025-02-05 00:16:46 +0530

    Fixes

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -3,7 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { User } from "../../models/User";
 
-export async function PUT() {
+export async function PUT(req) {
     mongoose.connect(process.env.MONGO_URL);
     const data = await req.json();
     const session = await getServerSession(authOptions);
