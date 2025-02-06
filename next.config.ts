2fbaf40a6306194b8212d77e55d182419d54c8d8
ASP-03 <ankit20038@gmail.com>
2025-02-06 23:30:34 +0530

    some additionas for saving the profile image

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '*googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'asp-food-ordering.s3.amazonaws.com'
+
+      },
     ]
   }
 };
