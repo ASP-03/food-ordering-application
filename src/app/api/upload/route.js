e0e1cf52faf0d10a44ee5b0a8ac35edff817995f
ASP-03 <ankit20038@gmail.com>
2025-02-06 22:23:04 +0530

    new files

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,3 +1,28 @@
-export function POST(req) {
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import uniqid from "uniqid";
+
+export async function POST(req) {
+    const data = await req.formData();
+    if(data.get('file')) {
+        const file = data.get('file');
+
+        const s3Client = new S3Client({
+            region: 'us-east-1',
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY,
+                secretAccessKey: process.env.AWS_SECRET_KEY,
+            }
+        })
+
+        const ext = file.name.split('.').slice(-1)[0];
+
+        const newFileName = uniqid() + '.' + ext;
+
+        s3Client.send(new PutObjectCommand({
+            Bucket: 'asp-food-ordering',
+            Key
+
+        }))
+    }
     return Response.json(true);
 }
