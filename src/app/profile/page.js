1c70115b9164c430b9b672d55b64060278e759c2
ASP-03 <ankit20038@gmail.com>
2025-02-12 13:51:14 +0530

    fixes

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,19 +1,16 @@
 'use client';
-import { useSession } from "next-auth/react"
-import { redirect } from "next/dist/server/api-utils";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import Infobox from "../components/layout/Infobox";
-import Successbox from "../components/layout/Succesbox";
-import toast from "react-hot-toast";
+import { useSession } from 'next-auth/react'
+import { redirect } from 'next/dist/server/api-utils';
+import Image from 'next/image';
+import { useEffect, useState } from 'react'; 
+import Infobox from '../components/layout/Infobox';
+import Successbox from '../components/layout/Succesbox';
+import toast from 'react-hot-toast';
 
 export default function profilePage() {
     const session = useSession();
     const [userName, setUserName] = useState('');
     const [image, setImage] = useState('');
-    const [profileSaved, setProfileSaved] = useState(false);
-    const [isSaving, setIsSaving] = useState(false);
-    const [isUploading, setIsUploading] = useState(false);
     const {status} = session;
 
     useEffect(() => {
@@ -25,16 +22,14 @@ export default function profilePage() {
 
     async function handleProfileUpdate(ev) {
         ev.preventDefault();
-        setProfileSaved(false);
-        setIsSaving(true);
+        toast('Saving...')
         const response = await fetch('/api/profile', {
             method: 'PUT',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({name:userName, image}),
         })
-        setIsSaving(false);
         if(response.ok){
-            setProfileSaved(true);
+            toast.success('Profile Saved!')
         }
     }
     async function handleFileChange(ev) {
@@ -42,12 +37,16 @@ export default function profilePage() {
             if(files?.length === 1) {
                 const data = new FormData;
                 data.append('file', files[0]);
-                toast("Uploading...");
+                toast('Uploading...');
                 const response = await fetch ('/api/upload', {
                     method: 'POST',
                     body: data,
                 })
-                toast.success("Uploaded!");
+                if (response.ok) {
+                    toast.success('Uploaded!');
+                } else {
+                    toast.error('Upload unsuccessfull');
+                }
                 const result = await response.json();
                 setImage(result.url);
             }
@@ -62,36 +61,28 @@ export default function profilePage() {
     }
 
     return (
-        <section className="mt-8">
-            <h1 className="mb-4 text-center text-red-600 text-4xl">
+        <section className='mt-8'>
+            <h1 className='mb-4 text-center text-red-600 text-4xl'>
             Profile
             </h1>
-            <div className="max-w-md mx-auto">
-                {profileSaved && (
-                    <Successbox>Profile Saved!</Successbox>
-                  )
-                }
-                {isSaving && (
-                    <Infobox>Saving...</Infobox>
-                )} 
-                
-                <div className="flex gap-4 items-center">
+            <div className='max-w-md mx-auto'>
+                <div className='flex gap-4 items-center'>
                    <div>
-                      <div className="p-2 rounded-lg relative max-w-[120px]">
+                      <div className='p-2 rounded-lg relative max-w-[120px]'>
                         {image && (
-                             <Image className="rounded-lg w-full h-full mb-1" src={image || "/default-avatar.png"}
+                             <Image className='rounded-lg w-full h-full mb-1' src={image || '/default-avatar.png'}
                              width={250} height={250} alt={'avatar'} />
                          )}
                         <label>
-                         <input type="file" className="hidden" onChange={handleFileChange}/>
-                         <span className="block border border-gray-300 rounded-lg p-2 text-center cursor-pointer">Edit</span>
+                         <input type='file' className='hidden' onChange={handleFileChange}/>
+                         <span className='block border border-gray-300 rounded-lg p-2 text-center cursor-pointer'>Edit</span>
                          </label>
                       </div>
                    </div>
-                   <form className="grow" onSubmit={handleProfileUpdate}>
-                      <input type="text" placeholder="First and last name" value={userName} onChange={ev => setUserName(ev.target.value)} />
-                      <input type="email" disabled={true} value={session.data.user.email} />
-                      <button type="submit">Save</button>
+                   <form className='grow' onSubmit={handleProfileUpdate}>
+                      <input type='text' placeholder='First and last name' value={userName} onChange={ev => setUserName(ev.target.value)} />
+                      <input type='email' disabled={true} value={session.data.user.email} />
+                      <button type='submit'>Save</button>
                    </form>
                 </div>
 
