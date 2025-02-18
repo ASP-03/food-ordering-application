546f3e108e25c5ea6484cb6baee486fd39ed8420
ASP-03 <ankit20038@gmail.com>
2025-02-19 01:05:09 +0530

    added labels, updates in global css

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -140,24 +140,36 @@ export default function profilePage() {
                         </div>
                     </div>
                     <form className='grow' onSubmit={handleProfileUpdate}>
+                        <label>First and last name</label>
                         <input 
                             type='text' placeholder='First and last name' 
                             value={userName} onChange={ev => setUserName(ev.target.value)} />
+                        <label>Email</label>    
                         <input type='email' disabled={true} value={session.data.user.email} />
+                        <label>Phone</label>
                         <input 
                             type='tel' placeholder='Phone' 
                             value={phone} onChange={ev => setPhone(ev.target.value)}/>
+                        <label>Street Address</label>    
                         <input 
                             type='text' placeholder='Street Address'
                             value={streetAddress} onChange={ev => setStreetAddress(ev.target.value)}/>
                         <div className='flex gap-2'>
+                            <div>
+                            <label>City</label>
                              <input style={{'margin':'0'}}
                                 type='text' placeholder='City'
                                 value={city} onChange={ev => setCity(ev.target.value)} />
+                            </div>
+                            <div>
+                            <label>Pin Code</label>   
                              <input style={{'margin':'0'}}
                                 type='text' placeholder='Pin Code'
                                 value={pinCode} onChange={ev => setPinCode(ev.target.value)} />
+                            </div> 
+                             
                         </div>
+                        <label>Country</label>
                         <input 
                                 type='text' placeholder='Country'
                                 value={country} onChange={ev => setCountry(ev.target.value)} />
