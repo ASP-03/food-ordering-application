668bdff795973f4c53c88f0ff10157dda165c957
ASP-03 <ankit20038@gmail.com>
2025-03-18 23:14:19 +0530

    changes for storing phone number in user infos

--- src/app/models/UserInfo.js
+++ src/app/models/UserInfo.js
@@ -2,6 +2,7 @@ import { model, models, Schema } from "mongoose";
 
 const UserInfoSchema = new Schema({
     email: {type: String, required: true},
+    phone: {type: String},
     streetAddress: {type: String},
     city: {type: String},
     pinCode: {type: String},
