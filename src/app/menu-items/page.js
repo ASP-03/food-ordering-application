b3014056427783681f1643bb972ffe670d75b323
ASP-03 <ankit20038@gmail.com>
2025-03-24 02:18:30 +0530

    some changes to menu items

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -4,37 +4,11 @@ import { adminInfo } from "../components/adminInfo";
 import EditImage from "../components/layout/EditImage";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 export default function MenuItemsPage() {
 
-    const [image, setImage] = useState('')
-    const [name, setName] = useState('')
-    const [description, setDescription] = useState('')
-    const [basePrice, setBasePrice] = useState('')
     const {loading, data} = adminInfo()
-
-    async function handleFormSubmit(ev) {
-        ev.preventDefault()
-        const data = {image, name, description, basePrice}
-        const savingPromise = new Promise(async (resolve, reject) => {
-            const response = await fetch('/api/menu-items', {
-                method: 'POST',
-                body: JSON.stringify(data),
-                    headers: {'Content-Type': 'application/json'},
-
-            })
-            if (response.ok)
-                resolve()
-            else
-                reject()
-        })
-
-        await toast.promise(savingPromise, {
-            loading: 'Saving this tast item',
-            success: 'Saved',
-            error: 'Error'
-        })
-    }
 
     if (loading) {
         return 'Loading user info...'
@@ -46,6 +20,15 @@ export default function MenuItemsPage() {
 
 
     return (
-        <div>menu items</div>
+        <section className="mt-8 max-w-md mx-auto">
+            <UserTabs isAdmin={true} />
+            <div className="mt-8">
+                <Link 
+                    className=""
+                    href={'/menu-items/new'}>
+                    Create a new menu item
+                </Link>
+            </div>
+        </section>
     )
 }
