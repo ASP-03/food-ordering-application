239662dac5bc7441743c0eec8a181d8c2320c193
ASP-03 <ankit20038@gmail.com>
2025-03-24 01:50:13 +0530

    changes

--- src/app/components/layout/EditImage.js
+++ src/app/components/layout/EditImage.js
@@ -75,13 +75,13 @@ export default function EditImage({ link, setLink }) {
                 <Image className="rounded-lg w-full h-full mb-1" src={link} width={250} height={250} alt="avatar" />
             ) : (
                 <div className="text-center bg-gray-200 p-4 text-gray-500 rounded-lg mb-1">
-                    No image selected
+                    No image
                 </div>
             )}
             <label>
                 <input type="file" className="hidden" onChange={handleFileChange} />
                 <span className="block border border-gray-300 rounded-lg p-2 text-center cursor-pointer">
-                    {loading ? "Uploading..." : "Edit"}
+                Change Image
                 </span>
             </label>
         </>
--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -1,10 +1,40 @@
 'use client';
 import UserTabs from "../components/layout/UserTabs";
 import { adminInfo } from "../components/adminInfo";
+import EditImage from "../components/layout/EditImage";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function MenuItemsPage() {
 
+    const [image, setImage] = useState('')
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [basePrice, setBasePrice] = useState('')
     const {loading, data} = adminInfo()
+
+    async function handleFormSubmit(ev) {
+        ev.preventDefault()
+        const data = {image, name, description, basePrice}
+        const savingPromise = new Promise(async (resolve, reject) => {
+            const response = await fetch('/api/menu-items', {
+                method: 'POST',
+                body: JSON.stringify(data),
+                    headers: {'Content-Type': 'application/json'},
+
+            })
+            if (response.ok)
+                resolve()
+            else
+                reject()
+        })
+
+        await toast.promise(savingPromise, {
+            loading: 'Saving this tast item',
+            success: 'Saved',
+            error: 'Error'
+        })
+    }
 
     if (loading) {
         return 'Loading user info...'
@@ -18,18 +48,31 @@ export default function MenuItemsPage() {
     return (
         <section className="mt-8">
             <UserTabs isAdmin={true} />
-            <form className="mt-8 max-w-md mx-auto">
-              <div className="flex items-start gap-4">
+            <form onSubmit={handleFormSubmit} className="mt-8 max-w-md mx-auto">
+              <div className="grid items-start gap-4" 
+                   style={{gridTemplateColumns:'.3fr .7fr'}}>
                 <div>
-                    image 
+                    <EditImage link={image} setLink={setImage} /> 
                 </div>
                 <div className="grow">
                     <label>Item name</label>
-                    <input type="text"/>
+                    <input 
+                        type="text"
+                        value={name}
+                        onChange={ev => setName(ev.target.value)}
+                    />
                     <label>Description</label>
-                    <input type="text"/>
+                    <input
+                        type="text"
+                        value={description}
+                        onChange={ev => setDescription(ev.target.value)}
+                    />
                     <label>Base price</label>
-                    <input type="text"/>
+                    <input
+                        type="text"
+                        value={basePrice}
+                        onChange={ev => setBasePrice(ev.target.value)}
+                    />
                     <button type="submit">Save</button>
                 </div>
               </div>
