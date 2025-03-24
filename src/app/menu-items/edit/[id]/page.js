1d29d404186e8f4649b82006e93ce70aca5451b4
ASP-03 <ankit20038@gmail.com>
2025-03-25 02:36:45 +0530

    new files, using functions to prevent code repetition

--- src/app/menu-items/edit/[id]/page.js
+++ src/app/menu-items/edit/[id]/page.js
@@ -6,33 +6,27 @@ import Link from "next/link";
 import { redirect, useParams } from "next/navigation";
 import { adminInfo } from "../../../components/AdminInfo";
 import Left from "../../../components/icons/Left";
-import EditImage from "../../../components/layout/EditImage";
+import MenuItemForm from "../../../components/layout/MenuItemForm";
 
 export default function EditMenuItemPage() {
 
         const {id} = useParams()
-        const [image, setImage] = useState('')
-        const [name, setName] = useState('')
-        const [description, setDescription] = useState('')
-        const [basePrice, setBasePrice] = useState('')
-        const [redirectToItems, setRedirectToItems] = useState('')    
+        const [menuItem, setMenuItem] = useState(null)
+        const [redirectToItems, setRedirectToItems] = useState(false)    
         const {loading, data} = adminInfo()
 
         useEffect(() => {
             fetch('/api/menu-items').then(res => {
                 res.json().then(items => {
                     const item = items.find(i => i._id === id)
-                    setImage(item.image)
-                    setName(item.name)
-                    setDescription(item.description)
-                    setBasePrice(item.basePrice)
+                    setMenuItem(item)
                 })
             })
         }, [])
     
-        async function handleFormSubmit(ev) {
+        async function handleFormSubmit(ev, data) {
             ev.preventDefault()
-            const data = {image, name, description, basePrice, _id:id}
+            data = {...data, _id:id}
             const savingPromise = new Promise(async (resolve, reject) => {
                 const response = await fetch('/api/menu-items', {
                     method: 'PUT',
@@ -76,35 +70,7 @@ export default function EditMenuItemPage() {
                         <span>Show all menu items</span>
                     </Link>
                 </div>
-                <form onSubmit={handleFormSubmit} className="mt-8 max-w-md mx-auto">
-                  <div className="grid items-start gap-4" 
-                       style={{gridTemplateColumns:'.3fr .7fr'}}>
-                    <div>
-                        <EditImage link={image} setLink={setImage} /> 
-                    </div>
-                    <div className="grow">
-                        <label>Item name</label>
-                        <input 
-                            type="text"
-                            value={name}
-                            onChange={ev => setName(ev.target.value)}
-                        />
-                        <label>Description</label>
-                        <input
-                            type="text"
-                            value={description}
-                            onChange={ev => setDescription(ev.target.value)}
-                        />
-                        <label>Base price</label>
-                        <input
-                            type="text"
-                            value={basePrice}
-                            onChange={ev => setBasePrice(ev.target.value)}
-                        />
-                        <button type="submit">Save</button>
-                    </div>
-                  </div>
-                </form>
+                <MenuItemForm menuItem={menuItem} onSubmit={handleFormSubmit}/>
             </section>
         )
     
