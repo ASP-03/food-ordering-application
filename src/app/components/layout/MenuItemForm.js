f6b8d0fe18e1bf863c172b5fc112fdc4829d4f88
ASP-03 <ankit20038@gmail.com>
2025-03-25 21:51:43 +0530

    Added functionality to add sizes and extra price of new menu items

--- src/app/components/layout/MenuItemForm.js
+++ src/app/components/layout/MenuItemForm.js
@@ -6,8 +6,24 @@ export default function MenuItemForm({onSubmit, menuItem}) {
     const [image, setImage] = useState(menuItem?.image || '')
     const [name, setName] = useState(menuItem?.name || '')
     const [description, setDescription] = useState(menuItem?.description || '')
-    const [basePrice, setBasePrice] = useState(menuItem?.basePrice || '')    
+    const [basePrice, setBasePrice] = useState(menuItem?.basePrice || '')
+    const[sizes, setSizes] = useState([])
 
+    function addSize() {
+        setSizes(oldSizes => {
+            return [...oldSizes, {name: '', price: 0}]
+        })
+
+    }
+
+    function editSize(ev, index, prop){
+        const newValue = ev.target.value
+        setSizes(prevSizes => {
+            const newSizes = [...prevSizes]
+            newSizes[index][prop] = newValue
+            return newSizes
+        })
+    }
     return (
         <form
             onSubmit={ev => onSubmit(ev, {image, name, description, basePrice})} className="mt-8 max-w-md mx-auto">
@@ -17,24 +33,55 @@ export default function MenuItemForm({onSubmit, menuItem}) {
                         <EditImage link={image} setLink={setImage} /> 
                     </div>
                     <div className="grow">
-                        <label>Item name</label>
+                        <label className="px-1">Item name</label>
                         <input 
                             type="text"
                             value={name}
                             onChange={ev => setName(ev.target.value)}
                         />
-                        <label>Description</label>
+                        <label className="px-1">Description</label>
                         <input
                             type="text"
                             value={description}
                             onChange={ev => setDescription(ev.target.value)}
                         />
-                        <label>Base price</label>
+                        <label className="px-1">Base price</label>
                         <input
                             type="text"
                             value={basePrice}
                             onChange={ev => setBasePrice(ev.target.value)}
                         />
+                        <div className="bg-gray-200 p-2 rounded-md mb-2">
+                            <label className="px-1">Sizes</label>
+                            {sizes?.length > 0 && sizes.map((size, index) => (
+                            <div key={index} className="flex gap-2">
+                                <div>
+                                    <label className="px-1">Size name</label>
+                                    <input 
+                                    type="text" 
+                                    placeholder="Size name" 
+                                    value={size.name} 
+                                    onChange={ev => editSize(ev, index, 'name')}
+                                    />
+                                </div>
+                                <div>
+                                    <label>Extra price</label>
+                                    <input 
+                                    type="text" 
+                                    placeholder="Extra price" 
+                                    value={size.price} 
+                                    onChange={ev => editSize(ev, index, 'price')}
+                                    />
+                                </div>
+                            </div>
+                           ))}
+
+                            <button
+                             type="button" 
+                             onClick={addSize}
+                             className="bg-white">Add item size
+                            </button>
+                        </div>
                         <button type="submit">Save</button>
                     </div>
                   </div>
