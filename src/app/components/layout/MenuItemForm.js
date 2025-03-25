e42be45e17b15934b8b9446d23eb8eb9f8d97acc
ASP-03 <ankit20038@gmail.com>
2025-03-26 01:48:20 +0530

    added section to add prices for extra toppings

--- src/app/components/layout/MenuItemForm.js
+++ src/app/components/layout/MenuItemForm.js
@@ -2,45 +2,47 @@ import { useState } from "react";
 import EditImage from "./EditImage";
 import MenuItemPriceProps from "./MenuItemPriceProps";
 
-export default function MenuItemForm({onSubmit, menuItem}) {
-
-    const [image, setImage] = useState(menuItem?.image || '')
-    const [name, setName] = useState(menuItem?.name || '')
-    const [description, setDescription] = useState(menuItem?.description || '')
-    const [basePrice, setBasePrice] = useState(menuItem?.basePrice || '')
-
+export default function MenuItemForm({ onSubmit, menuItem }) {
+    const [image, setImage] = useState(menuItem?.image || "");
+    const [name, setName] = useState(menuItem?.name || "");
+    const [description, setDescription] = useState(menuItem?.description || "");
+    const [basePrice, setBasePrice] = useState(menuItem?.basePrice || "");
+    const [sizes, setSizes] = useState([]);
+    const [addToppingsPrice, setAddToppingsPrice] = useState(menuItem?.addToppingsPrice || [])
 
     return (
         <form
-            onSubmit={ev => onSubmit(ev, {image, name, description, basePrice})} className="mt-8 max-w-md mx-auto">
-                  <div className="grid items-start gap-4" 
-                       style={{gridTemplateColumns:'.3fr .7fr'}}>
-                    <div>
-                        <EditImage link={image} setLink={setImage} /> 
-                    </div>
-                    <div className="grow">
-                        <label className="px-1">Item name</label>
-                        <input 
-                            type="text"
-                            value={name}
-                            onChange={ev => setName(ev.target.value)}
-                        />
-                        <label className="px-1">Description</label>
-                        <input
-                            type="text"
-                            value={description}
-                            onChange={ev => setDescription(ev.target.value)}
-                        />
-                        <label className="px-1">Base price</label>
-                        <input
-                            type="text"
-                            value={basePrice}
-                            onChange={ev => setBasePrice(ev.target.value)}
-                        />
-                        <MenuItemPriceProps/>
-                        <button type="submit">Save</button>
-                    </div>
-                  </div>
-                </form>
-    )
+            onSubmit={(ev) => onSubmit(ev, { image, name, description, basePrice, sizes, addToppingsPrice})}
+            className="mt-8 max-w-md mx-auto"
+        >
+            <div className="grid items-start gap-4" style={{ gridTemplateColumns: ".3fr .7fr" }}>
+                <div>
+                    <EditImage link={image} setLink={setImage} />
+                </div>
+                <div className="grow">
+                    <label className="px-1">Item name</label>
+                    <input type="text" value={name} onChange={(ev) => setName(ev.target.value)} />
+                    <label className="px-1">Description</label>
+                    <input type="text" value={description} onChange={(ev) => setDescription(ev.target.value)} />
+                    <label className="px-1">Base price</label>
+                    <input type="text" value={basePrice} onChange={(ev) => setBasePrice(ev.target.value)} />
+
+                    <MenuItemPriceProps 
+                        name={'Sizes'} 
+                        addLabel={'Add item size'} 
+                        sizes={sizes} 
+                        setSizes={setSizes} 
+                    />
+                    <MenuItemPriceProps 
+                        name={'Add Toppings'}
+                        addLabel={'Add toppings price'}
+                        sizes={addToppingsPrice}
+                        setSizes={setAddToppingsPrice}
+                    />                 
+
+                    <button type="submit">Save</button>
+                </div>
+            </div>
+        </form>
+    );
 }
--- src/app/components/layout/MenuItemPriceProps.js
+++ src/app/components/layout/MenuItemPriceProps.js
@@ -1,69 +1,64 @@
 import Trash from "../icons/Trash";
 import Plus from "../icons/Plus";
 
-export default function MenuItemPriceProps(props, setProps) {
-
+export default function MenuItemPriceProps({ name, addLabel, sizes, setSizes }) {
     function addProp() {
-        setProps(oldProps => {
-            return [...oldProps, {name:'', price:0}]
-        })
-    
+        setSizes((oldSizes) => [...oldSizes, { name: "", price: 0 }]);
     }
-    
-     function editProp(ev, index, prop){
-        const newValue = ev.target.value
-        setProps(prevSizes => {
-            const newSizes = [...prevSizes]
-            newSizes[index][prop] = newValue
-            return newSizes
-        })
+
+    function editProp(ev, index, prop) {
+        const newValue = ev.target.value;
+        setSizes((prevSizes) => {
+            const newSizes = [...prevSizes];
+            newSizes[index][prop] = newValue;
+            return newSizes;
+        });
     }
-    
+
     function removeProp(indexToRemove) {
-        setSizes(prev => prev.filter((v, index) => index !== indexToRemove))
+        setSizes((prevSizes) => prevSizes.filter((_, index) => index !== indexToRemove));
     }
 
     return (
         <div className="bg-gray-200 p-2 rounded-md mb-2">
-                            <label className="px-1">Sizes</label>
-                            {props?.length > 0 && props.map((size, index) => (
-                            <div key={index} className="flex gap-2">
-                                <div>
-                                    <label className="px-1">Size name</label>
-                                    <input 
-                                    type="text" 
-                                    placeholder="Size name" 
-                                    value={size.name} 
-                                    onChange={ev => editProp(ev, index, 'name')}
-                                    />
-                                </div>
-                                <div>
-                                    <label>Extra price</label>
-                                    <input 
-                                    type="text" 
-                                    placeholder="Extra price" 
-                                    value={size.price} 
-                                    onChange={ev => editProp(ev, index, 'price')}
-                                    />
-                                </div>
-                                <div>
-                                    <button 
-                                        type="button"
-                                        onClick={() => removeProp(index)}
-                                        className="bg-white h-10 mt-6 px-2">
-                                        <Trash />
-                                    </button>
-                                </div>
-                            </div>
-                           ))}
-
+            <label className="px-1">{name}</label>
+            {sizes?.length > 0 &&
+                sizes.map((size, index) => (
+                    <div key={index} className="flex gap-2">
+                        <div>
+                            <label className="px-1">Name</label>
+                            <input
+                                type="text"
+                                placeholder="Size name"
+                                value={size.name}
+                                onChange={(ev) => editProp(ev, index, "name")}
+                            />
+                        </div>
+                        <div>
+                            <label>Extra price</label>
+                            <input
+                                type="text"
+                                placeholder="Extra price"
+                                value={size.price}
+                                onChange={(ev) => editProp(ev, index, "price")}
+                            />
+                        </div>
+                        <div>
                             <button
-                                type="button" 
-                                onClick={addProp}
-                                className="bg-white items-center">
-                                <Plus className="w-5 h-5"/>
-                                Add item size
+                                type="button"
+                                onClick={() => removeProp(index)}
+                                className="bg-white h-10 mt-6 px-2"
+                            >
+                                <Trash />
                             </button>
                         </div>
-    )
+                    </div>
+                ))}
+
+            <button type="button" onClick={addProp} className="bg-white flex items-center gap-2 px-2 py-1">
+                <Plus className="w-5 h-5" />
+                {addLabel}
+            </button>
+        </div>
+    );
 }
