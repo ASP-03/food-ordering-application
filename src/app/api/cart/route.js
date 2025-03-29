444db9fa24808facbc34c242c794fea1225ab2e4
ASP-03 <ankit20038@gmail.com>
2025-03-30 01:39:54 +0530

    fixes

--- src/app/api/cart/route.js
+++ src/app/api/cart/route.js
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
-import connectDB from "../../../utils/db";
+import connectDB from "../../../utils/db"; // Fixed import path
 import { Cart } from "../../models/Cart";
 
 export async function GET(req) {
